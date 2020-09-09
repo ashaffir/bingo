71b91e7707fb533a631c088de7d1d249c679692f
ashaffir <ashaffir@gmail.com>
2020-09-09 16:44:27 +0300

    password flow fixes, added user info return

--- bingo_project/urls.py
+++ bingo_project/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 from game import views as game_views
 admin.site.site_header = 'Bingo Matrix'
@@ -10,8 +11,16 @@ urlpatterns = [
     path('payments/', include('payments.urls', namespace='payments')),
     path('api/', include('api.urls', namespace='api')),
     path('game/', include('game.urls', namespace='game')),
+    path('users/', include('users.urls', namespace='users')),
     path('api/users/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
     
+
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='users/password_change.html'), name='password_change'),
+    path('password_change/done', auth_views.PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'), name='password_change_done'),
+    path('password-reset/done', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/reset_password.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
+
     # AllAuth
     # path('accounts/', include('allauth.urls')),
 
